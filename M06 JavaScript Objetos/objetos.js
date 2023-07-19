f5bd227513aa876a3a3451a0d327e6e9f569e10e
jocho <jocho.ve@gmail.com>
2023-07-18 20:58:21 -0400

    Envio Homeworks

---
+++ M06 JavaScript Objetos/objetos.js
@@ -0,0 +1,49 @@
+// // Made by myself
+// var sport = {
+//     withBall: ["FootBall", "Basketball", "Volleyball"],
+//     noBall: ["Boxing", "Surf", "Trekking"]
+// };
+
+// var person = {
+//     name: 'Lucas',
+//     age: 26,
+//     studies: {isProgrammer: true}
+// };
+
+// person.name = 'Marcelo';
+// console.log(person.name);
+// console.log(person);
+
+// var myFunctions = {
+//     greeting: function() {
+//         console.log('Hello, how you doing?');
+//     }
+// };
+
+// myFunctions.greeting();
+
+//person
+
+//console.log(person.age);
+// console.log(sport);
+// console.log(person);
+// DOT - NOTATION 
+var atuendos = { 
+    manos: ['Guantes', 'Anillos'], 
+    pies: ['Zapatos', 'Calcetines']
+};
+
+var fiestas = {
+    cumple: ['Infantiles', 'Adultos'],
+    aniversario: ['Novios', 'Casados']
+};
+// BRACKET NOTATION
+
+atuendosBrackets['piernas'] = ['Bermudas', 'Pantalones'];
+
+fiestasBrackets['cumple'] = ['Infantiles', 'Adultos'];
+
+console.log(atuendos);
+console.log(fiestas);
+console.log(atuendosBrackets);
+console.log(fiestasBrackets);
